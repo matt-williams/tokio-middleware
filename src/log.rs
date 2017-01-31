e84204333896acef991741e50b233707ef8ae49e
Matt Williams <matwilliams@hotmail.com>
2017-01-31 07:04:58 +0000

    Add response and error logging

--- src/log.rs
+++ src/log.rs
@@ -1,5 +1,7 @@
 use tokio_service::Service;
 use std::fmt;
+use futures::{Future, Then, future};
+use futures::future::FutureResult;
 
 #[derive(Clone)]
 pub struct Log<S> {
@@ -15,14 +17,27 @@ impl<S> Log<S> {
 impl<S> Service for Log<S>
     where S: Service,
           S::Request: fmt::Debug,
+          S::Response: fmt::Debug,
+          S::Error: fmt::Debug
 {
     type Request = S::Request;
     type Response = S::Response;
     type Error = S::Error;
-    type Future = S::Future;
+    type Future = Then<S::Future,
+         FutureResult<S::Response, S::Error>,
+         fn(Result<S::Response, S::Error>) -> FutureResult<Self::Response, Self::Error>>;
 
     fn call(&self, request: Self::Request) -> Self::Future {
         println!("[REQUEST] {:?}", request);
-        self.upstream.call(request)
+
+        fn log_result<R: fmt::Debug, E: fmt::Debug>(result: Result<R, E>) -> FutureResult<R, E> {
+            match result {
+                Ok(ref response) => println!("[RESPONSE] {:?}", response),
+                Err(ref error) => println!("[ERROR] {:?}", error),
+            }
+            future::result(result)
+        }
+
+        self.upstream.call(request).then(log_result)
     }
 }
